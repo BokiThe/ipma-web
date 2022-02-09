f9a7623c30cd9ee71e1f3945f653bea9a99d0b7d
BokiThe <ristic1989@gmail.com>
2022-02-09 02:50:10 +0100

    Get to mknow, benefits,certification section added

--- src/screens/homePage/HomePage.js
+++ src/screens/homePage/HomePage.js
@@ -6,14 +6,16 @@ import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 import TextComponent from "../../elements/text/text";
 import header_image from "../../assets/ilustrations/header_image.png";
-
+import vector_icon from "../../assets/icons/vector_icon.png";
+import benefits_image from "../../assets/ilustrations/benefits_image.png";
+import ed_naughton_avatar from "../../assets/ilustrations/ed_naughton_avatar.png";
 
 const HomePage = () => {
   return (
     <Container fluid id="rootSection">
       {/* --- HEADER SECTION --- */}
       <Row id="headerSection">
-        <Col fluid className="col-5 justify-content-space-around">
+        <Col id="headerText" className="col-5">
           <TextComponent
             className="title"
             text="Get certified by the official project management body"
@@ -24,19 +26,85 @@ const HomePage = () => {
             every level. All our certification leels are thoroughly vetted
             before issuing. Our certifications are also trusted worldwide."
           />
-
-          <button className="btnLarge">Explore certifications</button>
-          <button className="btnLarge">Help me choose</button>
-          <div>
-            <a id="scrollDown" href="#">
-              Scroll down 
-            </a>
-          </div>
+          <Row>
+            <button className="btnLarge">Explore certifications</button>
+            <button className="btnLarge">Help me choose</button>
+          </Row>
+          <span id="scrollDown" onClick={() => console.log("skrollDown")}>
+            Scroll down <Image fluid src={vector_icon}></Image>
+          </span>
         </Col>
 
         <Col className="col-5">
           <Image fluid src={header_image} alt="header_image" />
         </Col>
+      </Row>
+      {/* --- GET TO KNOW US MORE --- */}
+      <Row>
+        <Col>
+          <TextComponent className="title-m" text="Get to know us more" />
+          <TextComponent
+            className="subtitle"
+            text="The mission and purpose of IPMA Ireland are to implement and maintain the IPMA® 4-Level Certification System in the Republic of Ireland.
+"
+          />
+        </Col>
+        <Col>
+          <Row className="justify-content-end">
+            <Col className="col-3 text-center">
+              <TextComponent className="title" text="30+" />
+              <TextComponent className="subtitle" text="Active years" />
+            </Col>
+            <Col className="col-3 text-center">
+              <TextComponent className="title" text="10k+" />
+              <TextComponent className="subtitle" text="Certified members" />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      {/* --- BENEFITS SECTION --- */}
+      <Row>
+        <Col>
+          <Image fluid src={benefits_image} alt="benefits_image" />
+        </Col>
+        <Col>
+          <TextComponent
+            className="title-m"
+            text="Benefits of an IPMA certification"
+          />
+          <TextComponent
+            className="subtitle"
+            text="Organisations and business of today need personnel with solid project management competence. So, certified project managers know how to deliver with quality, faster and less expensively. The IPMA certification is performance based and provides standards by which project management performance can be measured."
+          />
+          <div id="messageBauble">
+            <TextComponent
+              className="subtitle"
+              text="An IPMA Certification goes a long way in stablishing credibility, since you have participated in an internationally recognised certification programme."
+            />
+          </div>
+          <div id="asideAvatar">
+            <Image fluid src={ed_naughton_avatar} alt="ed_naughton_avatar" />
+            <div id="avatarText">
+              <TextComponent className="title-s" text="Ed Naugthon" />
+              <TextComponent className="subtitle" text="CEO & Founder" />
+            </div>
+          </div>
+        </Col>
+      </Row>
+      {/* --- PROJECT MENAGMENT CERTIFICATION LEVELS --- */}
+      <Row id="projectMngSection">
+        <Row>
+          <TextComponent
+            className="title-m"
+            text="Project management certification levels"
+          />
+        </Row>
+        <Row>
+          <TextComponent
+            className="subtitle"
+            text="IPMA offers an international, competency-based, four level certification (4-L-C) scheme. In your own country, in your own language, at your own competence level. "
+          />
+        </Row>
       </Row>
     </Container>
   );
