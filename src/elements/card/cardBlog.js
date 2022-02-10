745fe0b53fa62181b1bd2fa8aca7ac2e7d83fc5b
BokiThe <ristic1989@gmail.com>
2022-02-10 14:44:49 +0100

    Faq component, Blog card, blog section added

---
+++ src/elements/card/cardBlog.js
@@ -0,0 +1,18 @@
+import React from "react";
+import TextComponent from "../text/text";
+import Image from "react-bootstrap/Image";
+import "./card.css";
+
+const CardBlogComponent = (props) => {
+  return (
+    <div className={props.className}>
+      <Image className={props.imgClass} src={props.src} alt={props.alt} />
+      <TextComponent className="title-xs " text={props.title} />
+      <a href={props.href} id={props.linkId}>
+        {props.linkText}
+      </a>
+    </div>
+  );
+};
+
+export default CardBlogComponent;
